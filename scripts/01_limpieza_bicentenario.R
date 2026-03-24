c6760eea503d953ed2103a43de211395e4e30d6f
rjaralp <ri.jaralopez@gmail.com>
2026-03-24 15:50:40 -0300

    Actualizacion pagina

--- scripts/01_limpieza_bicentenario.R
+++ scripts/01_limpieza_bicentenario.R
@@ -18,5 +18,17 @@ bic <- read_dta("datos/raw/bicentenario_limpio.dta")
 glimpse(bic)
 names(bic)
 
+# Recodificación de religión
+bic <- bic %>%
+  mutate(
+    religion_cat = case_when(
+      religion_encuestado == 1 ~ "Católica",
+      religion_encuestado %in% c(2, 3) ~ "Evangélica",
+      religion_encuestado == 4 ~ "Otras religiones",
+      religion_encuestado %in% c(5, 6) ~ "Sin religión",
+      TRUE ~ NA_character_
+    )
+  )
+
 saveRDS(bic, "datos/processed/bicentenario_clean.rds")
 
